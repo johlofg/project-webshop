a86672860efa0cc1c6553278025c10d6f109f29a
johlofg <jh.lofgren@gmail.com>
2022-02-16 21:13:06 +0100

    removed non used files in repo

--- code/src/App.js
+++ code/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Provider } from "react-redux"
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import products from "./reducer/products"
 import cart from "./reducer/cart"
 
